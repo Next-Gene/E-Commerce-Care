ce1ec327c41b41567aa51d353334fffdb1139e9d
rowida mohamed <rowiyda11a@gmail.com>
2025-02-23 14:46:44 +0200

    r

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
+import { AboutComponent } from './features/pages/about/about.component';
+import { CartComponent } from './shared/components/ui/cart/cart.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
@@ -10,6 +12,26 @@ export const routes: Routes = [
         path: 'home',
         loadComponent: () => import('./features/pages/home/home.component')
             .then(c => c.HomeComponent)
+        
+    },
+
+    {
+        path: 'about',
+        loadComponent: () => import('./features/pages/about/about.component')
+            .then(c => c.AboutComponent)
+        
+    },
+    {
+        path: 'categories',
+        loadComponent: () => import('./features/pages/categories/categories.component')
+            .then(c => c.CategoriesComponent)
+        
+    },
+    {
+        path: 'cart',
+        loadComponent: () => import('./shared/components/ui/cart/cart.component')
+            .then(c => c.CartComponent)
+        
     },
 
 ];
