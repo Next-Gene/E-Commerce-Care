9b1af7d0f75ea28f19b14a16ac2d17bb9ab62346
Mostafa Algorithm <mostafa.algorithm@gmail.com>
2025-04-12 23:31:20 +0200

    finished single product page

--- src/app/features/pages/single-product/components/single-product-rating/single-product-rating.component.ts
+++ src/app/features/pages/single-product/components/single-product-rating/single-product-rating.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-single-product-rating',
-  imports: [CarouselModule, CommonModule],
+  imports: [CarouselModule, TranslateModule, CommonModule],
   templateUrl: './single-product-rating.component.html',
   styleUrl: './single-product-rating.component.scss'
 })
